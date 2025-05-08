4d0a99b7be0622832a42aea7aa7e8f4f62877a71
Mihai11235 <lacusteanuofficial@gmail.com>
2025-05-08 15:13:31 +0300

    m

--- src/test/module-info.java
+++
@@ -1,9 +0,0 @@
-module pizzashop.test {
-
-    requires pizzashop; // needed to access main code
-    requires org.junit.jupiter.api;
-    requires org.junit.jupiter.params;
-    requires org.junit.platform.commons;
-
-    opens pizzashop.service to org.junit.platform.commons;
-}
