c489cc24137488078bf7c5d5eff691e555699c42
Mihai11235 <lacusteanuofficial@gmail.com>
2025-05-08 14:41:56 +0300

    m

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,4 +10,5 @@ module pizzashop {
     exports pizzashop;
     opens pizzashop.controller to javafx.fxml;
     exports pizzashop.controller;
+    opens pizzashop.service to org.junit.platform.commons;
 }
